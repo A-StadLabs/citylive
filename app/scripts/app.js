2a40fe41c592f13fd7a3d776e28a509c9c177c6c
kingflurkel <mikethuy@gmail.com>
2014-11-05 15:59:39 +0100

    gelijk of dattet niets is

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,6 +1,13 @@
 (function(document) {
   'use strict';
 
+  var tabs = document.querySelector('paper-tabs');
+  var stage = document.querySelector('cl-stage');
+
+  tabs.addEventListener('core-select', function(){
+  	stage.selected = tabs.selected;
+  })
+
   document.addEventListener('polymer-ready', function() {
     // Perform some behaviour
     console.log('Polymer is ready to rock!');
